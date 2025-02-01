672ec90326473a130af92fd7ff5295abfb0eb63d
mohammad saad ali <mohammadsaadali786.92@gmail.com>
2025-02-01 16:10:33 +0500

    for loops

--- JavaScript/class 6/app.js
+++ JavaScript/class 6/app.js
@@ -39,7 +39,7 @@
 // for(var i=1; i <= 10; i++){
 //     document.write(`${user} x ${i} = ${user * i} <br>`)
 // }
-
+ 
 
 
 
---
+++ JavaScript/class 7/app.js
@@ -0,0 +1,64 @@
+// var firstNames = ["Saad", "Sufiyan", "Sattar", "Raja"];
+// var lastName = ['ABCD', 'XYZ', 'ghi']
+
+// for(var i=0; i < firstNames.length; i++){ // i=1 suf
+   
+//     for(var j=0; j < lastName.length; j++){
+//         document.write(firstNames[i] + " " + lastName[j] + "<br>")
+//     }
+
+// }
+
+
+
+// for(var i=0; i < firstNames.length; i++){
+//     document.write(firstNames[i] + " " + lastName + "<br>")
+// }
+
+
+
+
+// for(var i=0; i < firstNames.length; i++){
+//     document.write(firstNames[i] + "<br>")
+// }
+
+
+// for(var i=1; i<= 100; i = i+10){ 
+//         for(var j=i; j < i+10; j++){
+//             document.write(j + " ")
+//         }
+//         document.write("<br>")
+// }
+
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+// 1 2 3 4 5
+
+// for(var i=1; i<=5; i++){
+//     for(var j=1; j <= i; j++){
+//         document.write(j + " ")
+//     }
+//     document.write("<br>")
+// }
+
+// 5 5 5 5 5
+// 5 5 5 5 5
+// 5 5 5 5 5
+// 5 5 5 5 5
+// 5 5 5 5 5
+
+
+// 1 2 3 4 5
+// 1 2 3 4 5
+// 1 2 3 4 5
+// 1 2 3 4 5
+// 1 2 3 4 5
+
+
+// 5
+// 5 4
+// 5 4 3
+// 5 4 3 2
+// 5 4 3 2 1
