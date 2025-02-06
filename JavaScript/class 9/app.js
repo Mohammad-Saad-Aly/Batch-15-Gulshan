43425278a4b882567755b028913989059476da43
mohammad saad ali <mohammadsaadali786.92@gmail.com>
2025-02-06 17:00:07 +0500

    string methods

---
+++ JavaScript/class 9/app.js
@@ -0,0 +1,68 @@
+// var str = 'SAYLANI'
+// var convert = str[0].toUpperCase()
+// var convert2 = str.slice(1).toLowerCase()
+// var final = convert + convert2
+// document.write(final)
+
+// var convert = str.toLowerCase()
+// document.write(convert)
+
+// var userWord = prompt("Enter any word")
+// var firstAlpha = userWord.slice(0,1).toUpperCase()
+// var remainAlpha = userWord.slice(1).toLowerCase()
+// var final = firstAlpha + remainAlpha
+// document.write(final)
+
+// var cars = ['audi','bmw','mehran','alto','foxi','mehran']
+
+// var check = cars.lastIndexOf('mehran')
+
+// console.log(check)
+
+
+
+// for(var i=0; i < cars.length; i++){
+//     var a = cars[i][0].toUpperCase()
+//     var b = cars[i].slice(1).toLowerCase()
+//     var final = a + b 
+//     document.write( final + "<br>")
+// }
+
+
+
+
+// var userInp = prompt('Enter any car')
+// var firstAlpha = userInp.slice(0,1).toUpperCase()
+// var remainAlpha = userInp.slice(1).toLowerCase()
+// var final = firstAlpha + remainAlpha
+// var match = false
+
+// for(var i=0; i < cars.length; i++){
+//     if(final == cars[i]){
+//         match = true
+//         document.write(final + " car found")
+//         break
+//     }
+// }
+// if(match == false){
+//     document.write(final + ' car not found')
+// }
+
+// var mobile = prompt("Enter mobile name Or Model")
+// document.write(mobile.length)
+
+
+// var naam = 'pakistani'
+// var check = naam.charAt(3)
+// document.write(check)
+
+
+// var naam = 'smit '
+// var naam2 = 'saylani'
+// var check = naam.concat(naam2)
+
+// document.write(check)
+
+var str = 'hello world hello'
+var naya = str.replaceAll('hello', 'hyder')
+document.write(naya)
