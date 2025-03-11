69d347662a6beb2c9596aece8075f1cf4993683f
mohammad saad ali <mohammadsaadali786.92@gmail.com>
2025-03-11 15:36:48 +0500

    setting classes

---
+++ JavaScript/class 16/app.js
@@ -0,0 +1,23 @@
+// reading values
+// function foo(){
+//     var getInp = document.getElementById('inp')
+//     console.log(getInp.value)
+// }
+
+// setting values
+// function foo(){
+//     var getInp = document.getElementById('inp')
+//     getInp.value = 'hello world'
+// }
+
+// function imgchange(){
+//     var img = 'https://media.istockphoto.com/id/1681388313/vector/cute-baby-panda-cartoon-on-white-background.jpg?s=612x612&w=0&k=20&c=qFrzn8TqONiSfwevvkYhys1z80NAmDfw3o-HRdwX0d8='
+//     var tt = document.getElementById('tt')
+//     tt.src = ''
+//     // tt.src = img
+// }
+
+// function foo(){
+//     var getImg = document.getElementById('img')
+//     getImg.className += ' tt'
+// }
