04451dfdf0c9a30d4704e31ef4abf763a769716f
mohammad saad ali <mohammadsaadali786.92@gmail.com>
2025-05-10 15:39:05 +0500

    image upload

---
+++ JavaScript/class 26/app.js
@@ -0,0 +1,12 @@
+// function imageUpload() {
+//   var getImage = document.querySelector("#getImage");
+//   var HtmlImage = document.getElementById("image");
+//   HtmlImage.src = getImage.value;
+//   HtmlImage.setAttribute("width", "50%");
+// }
+
+// function uploadImage() {
+//   var getImage = document.querySelector("#image");
+//   var ImagetoUrl = URL.createObjectURL(getImage.files[0]);
+//   document.getElementById('img').src = ImagetoUrl
+// }
