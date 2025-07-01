ff1d5c6fb3b8c6ca97578ff3d0496e931fd79370
mohammad saad ali <mohammadsaadali786.92@gmail.com>
2025-07-01 03:53:28 -0700

    ecmascript class 3

---
+++ Ecmascript 6/class 3/app.js
@@ -0,0 +1,89 @@
+// let arr=[1,3,5,7,9]
+// let arr2=[]
+// let result=arr.map((cv,ci,a)=>{
+//     console.log(`current value: ${cv}, current index: ${ci}, array: ${a}`);
+//     return cv*2
+// })
+// console.log(result);
+// let result=arr.forEach((cv)=>{
+//     console.log(cv);
+//     return cv
+// })
+// console.log(result);
+// let result=arr.filter((cv)=>{
+//     return cv>=3
+// })
+// console.log(result)
+// let result=arr2.reduce((acc,cv,ci,a)=>{
+//     console.log(cv);
+//     return cv+acc
+// })
+// console.log(result)
+// let arr=[1,2,3]
+// let [a,,c]=arr
+// console.log(c);
+// var a=5
+// var b=10
+// console.log(b);
+// var [b,a]=[a,b]
+// console.log(b);
+// let obj={
+//     name:"john",
+//     age:30,
+// }
+// let {age:a,id:b=0}=obj
+// console.log(obj.age);
+// for( let k in obj){
+//     console.log(k, obj[k]);
+// }
+// let arr=[19,19,0,'a']
+// let result=arr.find((cv)=>{
+//     console.log('Hello');
+//     return cv==19
+// })
+// console.log(result);
+
+// console.log("task 1")
+// setTimeout(()=>{
+//     console.log("hello world")
+// }, 3000)
+// console.log("task 3")
+
+// let food = new Promise((resolve, reject)=>{
+//     let status = false
+//     if(status == true){
+//         resolve("food is ready")
+//     }
+//     else{
+//         reject("food is not ready")
+//     }
+// })
+
+// food.then((data)=>{
+//     console.log(data);
+// }).catch((err)=>{
+//     console.log(err);
+// })
+
+
+// fetch('https://jsonplaceholder.typicode.com/posts')
+// .then((sufiyan)=>{
+//     return sufiyan.json()
+// })
+// .then((data)=>{
+//     console.log(data)
+// })
+// .catch((err)=>{
+//     console.log(err)
+// })
+
+
+
+
+
+
+
+
+
+
+
